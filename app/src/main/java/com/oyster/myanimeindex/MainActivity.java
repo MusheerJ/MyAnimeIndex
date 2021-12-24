644d5713ba41ef475d363e9d2384666a1ac253a4
Rohit Jagdale <rohitjagadale4@gmail.com>
2021-12-24 11:17:38 +0530

    log d changed

--- app/src/main/java/com/oyster/myanimeindex/MainActivity.java
+++ app/src/main/java/com/oyster/myanimeindex/MainActivity.java
@@ -12,10 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hellomusheer();
     }
 
-    public static void hellomusheer(){
-        Log.d("string madhe kela", "hellomusheer: ");
-    }
 }
